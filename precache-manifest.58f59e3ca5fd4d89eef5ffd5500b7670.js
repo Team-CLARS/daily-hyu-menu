8013a32a0b66b69860b7beda7fffccac4cdd8aa0
Laniel Park <Laniel8177@gmail.com>
2023-11-11 12:52:39 +0900

    Updates

---
+++ precache-manifest.58f59e3ca5fd4d89eef5ffd5500b7670.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522dafb41e3dc76b02d2e88c28f01b0c",
+    "url": "/daily-hyu-menu/index.html"
+  },
+  {
+    "revision": "eaf0d4b1c06ab62c332f",
+    "url": "/daily-hyu-menu/static/css/main.b5031892.chunk.css"
+  },
+  {
+    "revision": "c2caa6c27cd00e68fde1",
+    "url": "/daily-hyu-menu/static/js/2.1e5e159c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/daily-hyu-menu/static/js/2.1e5e159c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07cf100dfaad7809ca0",
+    "url": "/daily-hyu-menu/static/js/3.3a7e133d.chunk.js"
+  },
+  {
+    "revision": "eaf0d4b1c06ab62c332f",
+    "url": "/daily-hyu-menu/static/js/main.646127dd.chunk.js"
+  },
+  {
+    "revision": "023f94ffe028e7e9a1ed",
+    "url": "/daily-hyu-menu/static/js/runtime-main.383681d3.js"
+  },
+  {
+    "revision": "0cffa7bfad77599712cd189bb5d34edf",
+    "url": "/daily-hyu-menu/static/media/app-icon.0cffa7bf.png"
+  },
+  {
+    "revision": "ce9a8993fb9450a27d73760805065ce7",
+    "url": "/daily-hyu-menu/static/media/banner1.ce9a8993.png"
+  },
+  {
+    "revision": "f5e31b4df5624804b3d99c5091aec05e",
+    "url": "/daily-hyu-menu/static/media/logo.f5e31b4d.png"
+  }
+]);

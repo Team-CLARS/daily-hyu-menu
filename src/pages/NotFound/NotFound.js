42682b73c96439537be7aff758f8fd7ee36921db
Laniel Park <Laniel8177@gmail.com>
2023-11-12 16:02:26 +0900

    fix : alt error

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
     <>
       <CenterWrapper>
         <h1>이런 페이지 없는뎁쇼</h1>
-        <img src={error} />
+        <img src={error} alt="404"/>
       </CenterWrapper>
     </>
   );

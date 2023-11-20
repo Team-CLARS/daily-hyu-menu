1abb2bf975ed037b567d1659bdd9a5d7506ef9e0
Hyeon Ho Park <106924371+Laniel88@users.noreply.github.com>
2023-11-21 01:20:46 +0900

    Update DownloadSection.js

--- src/components/DownloadSection/DownloadSection.js
+++ src/components/DownloadSection/DownloadSection.js
@@ -11,7 +11,7 @@ const DownloadSection = () => {
       <ImageContainer>
         <img src={appIcon} alt="appIcon" />
       </ImageContainer>
-      <DownloadVersion><p>오늘의 식냥 ver 0.1.2</p></DownloadVersion>
+      <DownloadVersion><p>오늘의 식냥 ver 0.1.3</p></DownloadVersion>
       <DownloadInfo><p>Android API 19+<br/>iOS, iPadOS 12.0+</p></DownloadInfo>
       <DownloadButtons>
         <a href="https://play.google.com/store/apps/details?id=com.hyu.clars.daily_hyu_menu_frontend" target="_blank" rel="noopener noreferrer">

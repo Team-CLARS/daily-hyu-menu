2c39d184e21cf1eca0267f4764ae6dae7103da68
Hyeon Ho Park <106924371+Laniel88@users.noreply.github.com>
2024-01-22 19:42:01 +0900

    Update Data.js

--- src/pages/ChangeLogs/Data.js
+++ src/pages/ChangeLogs/Data.js
@@ -1,6 +1,24 @@
 
 
 export const versions = [
+      {
+    date: "2024.01.22",
+    version: "1.0.0",
+    isPublic: true,
+    improvements: ["모든 OS 홈 위젯 제공", "과거 메뉴 기반 사진 조회 기능 추가", "내일/모레의 메뉴 조회 기능 추가", "UI 개선"],
+  },
+    {
+    date: "2024.01.16",
+    version: "0.1.5(Beta)",
+    isPublic: false,
+    improvements: ["홈 위젯 추가"],
+  },
+    {
+    date: "2023.12.30",
+    version: "0.1.4(Beta)",
+    isPublic: true,
+    improvements: ["UI 개선", "성능 개선"],
+  },
   {
     date: "2023.11.16",
     version: "0.1.3(Beta)",

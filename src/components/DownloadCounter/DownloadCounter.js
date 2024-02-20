11950647c909db63ba2ffe523863b9e516b24208
Hyeon Ho Park <106924371+Laniel88@users.noreply.github.com>
2024-02-21 05:17:54 +0900

    Update DownloadCounter.js

--- src/components/DownloadCounter/DownloadCounter.js
+++ src/components/DownloadCounter/DownloadCounter.js
@@ -7,7 +7,7 @@ const DownloadCounter = () => {
   const counterRef = useRef();
 
   const startCounting = () => {
-    const targetCount = 200;
+    const targetCount = 310;
     const displayCount = Math.floor(targetCount / 10) * 10;
 
     const duration = 1500;
